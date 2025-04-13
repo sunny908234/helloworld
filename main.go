624fad6b8c5ed264b21734385adfcb2ad6528ed2
vboxuser <vboxuser@aaaa.myguest.virtualbox.org>
2025-04-13 16:05:24 +0700

    Add sum a and b

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    fmt.println("Hello, world.")
+	fmt.Println("Hello world")
+
+	// sum a and b variable
+	var a, b int
+	a = 1
+	b = 2
+	fmt.Println("sum: ", a+b)
 }
